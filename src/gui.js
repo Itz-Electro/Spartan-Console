d855a8e2d2dbe9ab87473c2df88258615b5e2469
Electro <jerrytodd2005@gmail.com>
2023-01-06 20:20:02 -0600

    ui update

--- src/gui.js
+++ src/gui.js
@@ -1,16 +1,37 @@
+const recentGames = document.getElementById('recent-plays')
+const loadButton = document.getElementById('loadButton')
+const gamelist = document.getElementById('game-list-content')
 
 
 
-addRecentGame('test')
-
-
+addRecentGame('Place Holder Game')
 function addRecentGame(title) {
-    const recentGames = document.getElementById('recent-plays')
+    
     let game = document.createElement('div')
     game.classList.add('recent-game')
     game.append(
         document.createTextNode(title)
     )
-    console.log(game)
     recentGames.append(game)
+}
+
+detectGames()
+function addGame(title) {
+    let game = document.createElement('li')
+    let buttonElement = '<button type="button" class="delete-game">Delete</button>'
+    game.innerHTML = title
+    //game.innerHTML += buttonElement
+    game.classList.add('game-item')
+    gamelist.append(game)
+
+    return
+    const deleteButton = game.querySelector('.delete-game');
+    game.addEventListener('click', event => {
+        gamelist.removeChild(game);
+    });
+}
+function detectGames() {
+    for (var i=0; i<5; i++) {
+        addGame(`Place Holder ${i+1}`)
+    }
 }
